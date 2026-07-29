4d6d1d4d3a1cd211635cd3d7bfbd8448881e0a68
Nicola Cattaneo <nicolacatdal@gmail.com>
2026-07-29 10:20:14 -0600

    365 authentication module

---
+++ src/auth.py
@@ -0,0 +1,30 @@
+import msal
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+TENANT_ID = os.getenv("TENANT_ID")
+D365_RESOURCE_URL = os.getenv("D365_RESOURCE_URL")
+D365_DATA_URL = os.getenv("D365_DATA_URL")
+
+AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
+
+app = msal.ConfidentialClientApplication(
+    client_id=CLIENT_ID,
+    client_credential=CLIENT_SECRET,
+    authority=AUTHORITY,
+)
+
+SCOPE = [f"{D365_RESOURCE_URL}/.default"]
+
+result = app.acquire_token_for_client(scopes=SCOPE)
+assert result is not None
+
+if "access_token" in result:
+    token = result["access_token"]
+else:
+    print(result.get("error"))
+    print(result.get("error_description"))
